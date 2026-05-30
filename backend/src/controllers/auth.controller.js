683fdf752b9f5ca6a91fdf3eb68116deea829997
Yashmendra Bais <yashmendrabais@gmail.com>
2026-05-30 15:11:06 +0530

    fix auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ const { successResponse, errorResponse } = require('../utils/response.utils');
 
 const cookieOptions = {
 	httpOnly: true,
-	secure: process.env.NODE_ENV === 'production',
+	secure: true,
 	sameSite: 'none',
 	maxAge: 7 * 24 * 60 * 60 * 1000,
 };
