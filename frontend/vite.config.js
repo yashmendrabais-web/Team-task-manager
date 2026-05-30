5fa15fbda14eccdb38073ba2ed3a3b2495754541
Yashmendra Bais <yashmendrabais@gmail.com>
2026-05-30 17:40:58 +0530

    fix auth controller

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://backend-production-4171.up.railway.app/api')
+  }
 })
